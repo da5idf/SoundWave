be961fe2612711bdab1de15f2ef05e619abe64af
da5idf <davidpforster24@gmail.com>
2022-05-05 16:39:40 -0400

    homepage layout done

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,17 +2,22 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import './HomePage.css'
+import SignupFormModal from '../SignupFormPage'
 import * as trackActions from '../../store/track'
 
 function HomePage() {
     const dispatch = useDispatch();
 
     const trackObjs = useSelector((state) => state.tracks);
+    const sessionUser = useSelector((state) => state.session.user);
+    console.log("sessionUser", sessionUser)
     const tracks = Object.values(trackObjs);
 
-    useEffect(async () => {
-        await dispatch(trackActions.getTracks());
+    useEffect(() => {
+        dispatch(trackActions.getTracks());
     }, [dispatch]);
+
+    const buttonText = 'Start uploading today';
 
     return (
         <div id="homepage-container">
@@ -20,17 +25,24 @@ function HomePage() {
                 <div id="cover-image-container">
                     <img src={require("../../images/CoverImages/cover_image1.jpeg")} id="cover-image" />
                     <div id="cover-image-headers">
-                        <h2>Hop on a SoundWave</h2>
-                        <h3>Upload your first track and begin your journey. SoundCloud gives you space to create, find your fans, and connect with other artists.</h3>
-                        <button className="button" id="cover-image-button">Start uploading today</button>
+                        <h2 id="main-title">Hop on a SoundWave</h2>
+                        <h3 id="scroll-info">Upload your first track and begin your journey. SoundCloud gives you space to create, find your fans, and connect with other artists.</h3>
+                        {!sessionUser && <SignupFormModal buttonText={buttonText} />}
+                        {/* <button
+                            className="button"
+                            id="cover-image-button"
+                        // onClick={ }
+                        >
+                            Start uploading today
+                        </button> */}
                     </div>
                 </div>
                 <div id="homepage-song-feed">
-                    <div id="feed-title">Hear trending songs for free in the SoundCloud community</div>
+                    <div id="feed-title">Hear trending songs for free from the SoundCloud community</div>
                     <div id="feature-tracks">
                         {
                             tracks.length && tracks.map(track => (
-                                <div id={track.id}>{track.name}</div>
+                                <div key={track.id}>{track.name}</div>
                             ))
                         }
                     </div>
