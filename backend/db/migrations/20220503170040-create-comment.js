ee09a6a25551694c5a14fa22e29778378dbfaa88
da5idf <davidpforster24@gmail.com>
2022-05-07 13:57:26 -0400

    relations fixed

--- backend/db/migrations/20220503170040-create-comment.js
+++ backend/db/migrations/20220503170040-create-comment.js
@@ -14,11 +14,13 @@ module.exports = {
       },
       userId: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: "Users" }
       },
       trackId: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: "Tracks" }
       },
       createdAt: {
         allowNull: false,
