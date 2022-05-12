bdf3058eee2c47a99ef38a873862a5c96b3f372c
da5idf <davidpforster24@gmail.com>
2022-05-12 13:18:01 -0400

    another try at audio cors fix

--- frontend/src/components/WaveForm/index.js
+++ frontend/src/components/WaveForm/index.js
@@ -18,12 +18,12 @@ function WaveForm({ url }) {
             hideScrollbar: true,
             xhr: {
                 cache: "default",
-                mode: "cors",
-                method: "GET",
-                headers: [
-                    { key: "cache-control", value: "no-cache" },
-                    { key: "pragma", value: "no-cache" }
-                ]
+                mode: "no-cors",
+                // method: "GET",
+                // headers: [
+                //     { key: "cache-control", value: "no-cache" },
+                //     { key: "pragma", value: "no-cache" }
+                // ]
             },
         });
 
