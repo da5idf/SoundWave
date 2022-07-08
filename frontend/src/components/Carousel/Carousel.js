511e67d4e3c4209fcb45c00527fc858aa5c63b18
da5idf <davidpforster24@gmail.com>
2022-07-08 13:32:09 -0400

    scroll adjustments

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from "uuid";
 
 import './Carousel.css';
@@ -7,24 +7,31 @@ import TrackCard from "../TrackCard"
 export default function Carousel({ tracks }) {
     const genre = tracks[0]?.Genre.name;
 
+    const [scroll, setScroll] = useState(0);
+    const scrollPxls = 740
+
     // get carousel tracks container for the two functions below
     const moveLeft = (e) => {
         const carousel = document.getElementById(`${genre}-carousel`);
-        carousel.scrollLeft -= 800
+        carousel.scrollLeft -= scrollPxls
+        setScroll(prev => prev - scrollPxls);
     }
 
     const moveRight = () => {
         const carousel = document.getElementById(`${genre}-carousel`);
-        carousel.scrollLeft += 800
+        carousel.scrollLeft += scrollPxls
+        setScroll(prev => prev + scrollPxls)
     }
 
     return (
         <div className="carousel-container">
-            <div className="carousel-button-container carousel-left" onClick={moveLeft}>
-                <div className="carousel-button-box">
-                    <i className="fa-solid fa-chevron-left"></i>
+            {scroll !== 0 &&
+                <div className="carousel-button-container carousel-left" onClick={moveLeft}>
+                    <div className="carousel-button-box">
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </div>
                 </div>
-            </div>
+            }
             <div id={`${genre}-carousel`} className="carousel-tracks">
                 {
                     tracks.map(track => {
